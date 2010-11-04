c05822858cbcbb60e3155da1ca877dd51aa238b9
Sam Phillips <samdphillips@gmail.com>
2010-11-04 14:13:16 -0400

    Removed compilation methods.

--- lib/onyx/nodes.rb
+++ lib/onyx/nodes.rb
@@ -24,21 +24,6 @@ module Onyx
     end
 
     class ExprNode < ParseNode
-        def block?
-            false
-        end
-
-        def compile
-            expand
-            cg = CodeGen.new
-            gen_value_code(cg)
-            cg
-        end
-
-        def gen_effect_code(cg)
-            gen_value_code(cg)
-            cg.pop
-        end
     end
 
     class SeqNode < ExprNode
@@ -46,18 +31,6 @@ module Onyx
 
         def initialize(exprs)
             @exprs = exprs
-        end
-
-        def expand
-            @exprs.each {|e| e.expand}
-        end
-
-        def gen_value_code(cg)
-            (0...@exprs.size - 1).each do |i|
-                @exprs[i].gen_effect_code(cg)
-            end
-
-            @exprs.last.gen_value_code(cg)
         end
     end
 
@@ -67,9 +40,6 @@ module Onyx
         def initialize(var)
             @var = var
         end
-
-        def expand
-        end
     end
 
     class ConstNode < ExprNode
@@ -78,49 +48,15 @@ module Onyx
         def initialize(value)
             @value = value
         end
-
-        def expand
-        end
-
-        def gen_value_code(cg)
-            cg.push_const(@value)
-        end
     end
 
     class MessageNode < ExprNode
         attr_reader :rcvr, :msg, :args
 
-        SpecialMessage = {}
-
-        def self.register_special(message, klass)
-            SpecialMessage[message] = klass
-        end
-
         def initialize(rcvr, msg, args)
             @rcvr     = rcvr
             @msg      = msg
             @args     = args
-            @expanded = nil
-        end
-
-        def expand
-            if SpecialMessage.include?(@msg) then
-                @expanded = SpecialMessage[@msg].expand(self)
-            end
-
-            if @expanded.nil? then
-                @rcvr.expand
-                @args.each {|a| a.expand}
-            end
-        end
-
-        def gen_value_code(cg)
-            if @expanded.nil? then
-                # normal send
-                gotta_write
-            else
-                @expanded.gen_value_code(cg)
-            end
         end
     end
 
@@ -130,24 +66,15 @@ module Onyx
         def initialize(expr)
             @expr = expr
         end
-
-        def expand
-            @expr.expand
-        end
     end
 
     class BlockNode < ExprNode
-        attr_reader :args, :temps, :stmts, :free
+        attr_reader :args, :temps, :stmts
 
         def initialize(args, temps, stmts)
             @args = args
             @temps = temps
             @stmts = stmts
-            @free  = []
-        end
-
-        def block?
-            true
         end
     end
     
@@ -168,13 +95,6 @@ module Onyx
             @args = args
             @temps = temps
             @stmts = stmts
-        end
-
-        def compile
-            @stmts.expand
-            cg = CodeGen.new
-            @stmts.gen_value_code(cg)
-            cg
         end
     end
 end
