27aae6ee711db4c99fddeaebd039483cdb501866
Sam Phillips <samdphillips@gmail.com>
2010-11-04 13:02:06 -0400

    Removed files not needed for interpreter.

--- lib/onyx.rb
+++ lib/onyx.rb
@@ -1,12 +1,8 @@
 
-require 'onyx/codegen'
 require 'onyx/env'
 require 'onyx/lexer'
 require 'onyx/nodes'
-require 'onyx/control_nodes'
-require 'onyx/prim_nodes'
 require 'onyx/obj/class'
 require 'onyx/obj/method'
 require 'onyx/parser'
-require 'onyx/vm'
 
--- t/tests.rb
+++ t/tests.rb
@@ -2,5 +2,4 @@
 require 'test/unit'
 require 't/test_lexer'
 require 't/test_parser'
-require 't/test_vm'
 
