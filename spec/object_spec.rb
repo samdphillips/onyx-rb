3b46a88659bae1dd938d4d4921715a3fb729f257
Sam Phillips <samdphillips@gmail.com>
2012-06-06 22:17:43 -0700

    Added Object>>isMemberOf:

--- spec/object_spec.rb
+++ spec/object_spec.rb
@@ -12,5 +12,22 @@ describe Onyx::Interpreter do
     it "class of an object is Object" do
         should interpret('Object new class', subject.globals[:Object])
     end
+
+    it "object is a member of object" do
+        should interpret('Object new isMemberOf: Object', true)
+    end
+
+    it "object is not a member of Array" do
+        should interpret('Object new isMemberOf: Array', false)
+    end
+
+    it "Array object is a member of Array" do
+        should interpret('(Array new: 8) isMemberOf: Array', true)
+    end
+
+    it "Array object is not a member of Object" do
+        should interpret('(Array new: 8) isMemberOf: Object', false)
+    end
+
 end
 
