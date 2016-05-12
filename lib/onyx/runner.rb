8627912425d3773ebcef45b3353aa0e27f549686
Sam Phillips <samdphillips@gmail.com>
2016-05-11 18:02:31 -0700

    Nicer exception messages

--- lib/onyx/runner.rb
+++ lib/onyx/runner.rb
@@ -2,10 +2,15 @@
 module Onyx
     class Runner
         def self.run(args)
-            terp = Interpreter.boot
-            args.each do |f|
-                node = Parser.parse_file(f)
-                terp.eval(node)
+            begin
+                terp = Interpreter.boot
+                args.each do |f|
+                    node = Parser.parse_file(f)
+                    terp.eval(node)
+                end
+            rescue OnyxError => e
+                puts 'Exception!'
+                puts e
             end
         end
     end
